ac79005933f99ba31937c9c106d069a32a903a05
Aviithapa <abhishekthapa115@gmail.com>
2024-06-30 08:38:52 +0545

    events

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,22 @@
 // import Image from "next/image";
 // import logo from "../../public/assests/logo.png";
-"use client"
+"use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 function Navbar() {
+  const router = useRouter();
+  const [accessToken, setAccessToken] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) setAccessToken(token);
+  }, [accessToken, localStorage]);
 
- const router = useRouter();
-const [accessToken, setAccessToken] = useState("");
- useEffect(() => {
-  const token = localStorage.getItem("accessToken");
-  if(token)
-  setAccessToken(token);
- }, [accessToken, localStorage])
-
-
- const handleLogout = () => {
-  localStorage.removeItem('accessToken');
-  location.reload();
- }
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    location.reload();
+  };
   return (
     <header className="flex justify-between items-center h-header-height gap-5 bg-slate-50 shadow-md fixed top-0 w-full  z-50">
       {/* logo  */}
@@ -40,6 +37,12 @@ const [accessToken, setAccessToken] = useState("");
         <Link href="/events" className="hover:text-green-400 text-xl">
           Events
         </Link>
+        <Link
+          href="/dashboard/create-event"
+          className="hover:text-green-400 text-xl"
+        >
+          Events Create
+        </Link>
         <Link href="/About" className="hover:text-green-400 text-xl">
           About Us
         </Link>
@@ -52,27 +55,25 @@ const [accessToken, setAccessToken] = useState("");
         <Link href="/donate" className="hover:text-green-400 text-xl">
           Donate
         </Link>
-        {accessToken && 
-         <button onClick={handleLogout}>Logout</button>
-        }
-
+        {accessToken && <button onClick={handleLogout}>Logout</button>}
       </nav>
       {/* login button  */}
       <div className="  bg-transparent w-[150px]">
-        {accessToken ? 
-        <Link
-          className="p-4 rounded-xl border w-16 space-x-1 h-10 bg-blue hover:bg-zinc-300"
-          href="/dashboard"
-        >
-          <span className="text-xl">Dashboard</span>
-        </Link>  :
-        <Link
-          className="p-4 rounded-xl border w-16 space-x-1 h-10 bg-blue hover:bg-zinc-300"
-          href="/login"
-        >
-          <span className="text-xl">Login</span>
-        </Link>
-        }
+        {accessToken ? (
+          <Link
+            className="p-4 rounded-xl border w-16 space-x-1 h-10 bg-blue hover:bg-zinc-300"
+            href="/dashboard"
+          >
+            <span className="text-xl">Dashboard</span>
+          </Link>
+        ) : (
+          <Link
+            className="p-4 rounded-xl border w-16 space-x-1 h-10 bg-blue hover:bg-zinc-300"
+            href="/login"
+          >
+            <span className="text-xl">Login</span>
+          </Link>
+        )}
       </div>
     </header>
   );
